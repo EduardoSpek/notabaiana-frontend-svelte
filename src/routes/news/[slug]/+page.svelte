6f0d21b262531af51f875363bd9a500b17e2ef67
eduardospek <spektogran@gmail.com>
2024-04-19 15:31:17 -0300

    add: Adicionada a data da notícia

--- src/routes/news/[slug]/+page.svelte
+++ src/routes/news/[slug]/+page.svelte
@@ -2,6 +2,19 @@
 	import { getContext } from 'svelte';
 	export let data;
 	const host = getContext('host');
+	let data_news = new Date(Date.parse(data.item.created_at));
+
+	function formatarData(data) {
+		const dia = data.getDate().toString().padStart(2, '0');
+		const mes = (data.getMonth() + 1).toString().padStart(2, '0');
+		const ano = data.getFullYear();
+		const hora = data.getHours().toString().padStart(2, '0');
+		const minuto = data.getMinutes().toString().padStart(2, '0');
+		const segundo = data.getSeconds().toString().padStart(2, '0');
+
+		return `${dia}/${mes}/${ano} às ${hora}:${minuto}:${segundo}`;
+	}
+	data_news = formatarData(data_news);
 </script>
 
 <svelte:head>
@@ -13,6 +26,9 @@
 	<div class="title">
 		<h3>{data.item.title}</h3>
 	</div>
+
+	<div class="data_atual">Publicado em {data_news}</div>
+
 	<div class="img">
 		<img class="thumb" src="{host}/images/{data.item.image}" alt={data.item.title} />
 	</div>
@@ -48,5 +64,12 @@
 	.thumb {
 		border-radius: 4px;
 		width: 100%;
+	}
+	.data_atual {
+		font-family: 'Cabin', sans-serif;
+		font-size: 14px;
+		color: var(--cinza-escuro);
+		margin-top: -24px;
+		margin-bottom: 20px;
 	}
 </style>
