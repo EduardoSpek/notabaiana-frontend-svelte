b94e531af428993872a73934c988efcbea3c3fa3
Eduardo Spek <spektogran@gmail.com>
2024-05-09 22:04:07 -0300

    Atualizar o +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script>
+	import { onMount } from 'svelte';
 	import '../global.css';
 	import '@fontsource-variable/roboto-condensed';
 	import '@fontsource-variable/cabin';
 	import '@fontsource/poppins';
 	import Topo from '$lib/Topo.svelte';
+
+function checkAndInitWidgets() {
+    if (typeof twttr !== 'undefined' && twttr.widgets && typeof twttr.widgets.load === 'function') {
+      twttr.widgets.load();
+    }
+    
+  }
+
+  onMount(() => {
+    checkAndInitWidgets();
+  });
 </script>
 
 <Topo />
