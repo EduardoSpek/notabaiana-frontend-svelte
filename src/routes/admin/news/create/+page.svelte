8d85f74fc53f42bdf91ba23b50624bbe80cfc797
Eduardo Spek <spektogran@gmail.com>
2024-05-29 02:48:57 -0400

    add: Adicionada rota para criar notícia

---
+++ src/routes/admin/news/create/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { HOST_API } from '$lib/index';
+</script>
+
+<section>
+	<div class="central">
+		<div class="conteudo">
+			<form action="{HOST_API}/news/create" method="post" enctype="multipart/form-data">
+				<label for="keyinput">Key:</label><br />
+				<input type="text" class="inputForm" id="keyinput" name="key" /><br /><br />
+
+				<label for="cInput">Categoria:</label><br />
+				<input type="text" class="inputForm" id="cInput" name="category" /><br /><br />
+
+				<label for="tinput">Título:</label><br />
+				<input type="text" class="inputForm" size="70" id="tinput" name="title" /><br /><br />
+
+				<label for="ttextarea">Texto:</label><br />
+				<textarea name="text" id="ttextarea" class="textareaForm"></textarea><br /><br />
+
+				<label for="fileInput">Selecione um arquivo:</label><br />
+				<input type="file" id="fileInput" name="Image" class="fileForm" /><br /><br />
+				<input type="submit" value="Enviar Arquivo" class="buttonForm" />
+			</form>
+		</div>
+	</div>
+</section>
