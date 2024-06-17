b02616ed6a8716c6aded2b8a1b25e7d6dfe3e8a7
Eduardo Spek <spektogran@gmail.com>
2024-06-16 22:07:23 -0300

    refact: Adicionado Promise.all para acelerar o carregamento da home

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,20 +1,62 @@
 import { HOST_API } from '$lib/index.js';
 
 export async function load({ fetch }) {
+	let news;
+	let famosos;
+	let esportes;
+	let municipios;
+	let justica;
 
-	const news = await fetch(`${HOST_API}/top`).then((res) => {
-		return res.json();
+	const fnNews = () => {
+		return fetch(`${HOST_API}/top`).then((res) => {
+			return res.json();
+		});
+	};
+
+	const fnFamosos = () => {
+		return fetch(`${HOST_API}/news/category/famosos/1`).then((res) => {
+			return res.json();
+		});
+	};
+
+	const fnEsportes = () => {
+		return fetch(`${HOST_API}/news/category/esportes/1`).then((res) => {
+			return res.json();
+		});
+	};
+
+	const fnMunicipios = () => {
+		return fetch(`${HOST_API}/news/category/municipios/1`).then((res) => {
+			return res.json();
+		});
+	};
+
+	const fnJustica = () => {
+		return fetch(`${HOST_API}/news/category/justica/1`).then((res) => {
+			return res.json();
+		});
+	};
+
+	const allPromises = Promise.all([
+		fnNews(),
+		fnFamosos(),
+		fnEsportes(),
+		fnMunicipios(),
+		fnJustica()
+	]);
+
+	await allPromises.then(([rnews, rfamosos, resportes, rmunicipios, rjustica]) => {
+		news = rnews;
+		famosos = rfamosos;
+		esportes = resportes;
+		municipios = rmunicipios;
+		justica = rjustica;
 	});
-
 
 	news.forEach((item, i) => {
 		if (item.title_ai) {
 			news[i].title = news[i].title_ai;
 		}
-	});
-
-	const famosos = await fetch(`${HOST_API}/news/category/famosos/1`).then((res) => {
-		return res.json();
 	});
 
 	famosos.news.forEach((item, i) => {
@@ -23,28 +65,16 @@ export async function load({ fetch }) {
 		}
 	});
 
-	const esportes = await fetch(`${HOST_API}/news/category/esportes/1`).then((res) => {
-		return res.json();
-	});
-
 	esportes.news.forEach((item, i) => {
 		if (item.title_ai) {
 			esportes.news[i].title = esportes.news[i].title_ai;
 		}
 	});
 
-	const municipios = await fetch(`${HOST_API}/news/category/municipios/1`).then((res) => {
-		return res.json();
-	});
-
 	municipios.news.forEach((item, i) => {
 		if (item.title_ai) {
 			municipios.news[i].title = municipios.news[i].title_ai;
 		}
-	});
-
-	const justica = await fetch(`${HOST_API}/news/category/justica/1`).then((res) => {
-		return res.json();
 	});
 
 	justica.news.forEach((item, i) => {
