d98eb46b1ae12ff7aba849bcb8889eafdaa275b7
Eduardo Spek <spektogran@gmail.com>
2024-07-15 01:49:47 -0300

    add: Adicionada página 404 em News/slug

---
+++ src/routes/news/[slug]/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { SITE_NAME } from '$lib/index.js';
+</script>
+
+<svelte:head>
+	<title>Página não encontrada ou removida - {SITE_NAME}</title>
+	<meta name="description" content="Error 404 - Página não encontrada ou removida" />
+</svelte:head>
+<section>
+	<div class="central">
+		<div class="conteudo">
+			<h2>Página não encontrada</h2>
+
+			<p>A página que você tentou acessar não existe ou foi removida do nosso site.</p>
+		</div>
+	</div>
+</section>
