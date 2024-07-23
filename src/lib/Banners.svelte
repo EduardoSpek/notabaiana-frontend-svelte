b95a0de3e06a2be7b5f95ddd24c227077a711c46
Eduardo Spek <spektogran@gmail.com>
2024-07-22 23:04:23 -0300

    fix: Corrigido bug no link do banner

--- src/lib/Banners.svelte
+++ src/lib/Banners.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { HOST_API } from '../lib/index';
+	import { HOST_API } from '$lib/index.js';
 	export let banners;
 	export let region;
 	let bannersTopo;
@@ -14,43 +14,50 @@
 {#if bannersTopo.length > 0}
 	<section>
 		<div class="conteudo-flow">
-			<div class="b-base">
-				<div class="banner">
-					{#if bannersTopo[0].html == ''}
-						<a href={bannersTopo[0].link} target="_blank" class="link">
-							{#if bannersTopo[0].banner_pc != ''}
-								<img
-									src={HOST_API + bannersTopo[0].banner_pc}
-									alt={bannersTopo[0].title}
-									title={bannersTopo[0].title}
-									width="100%"
-									class="b-pc"
-								/>
-							{/if}
-							{#if bannersTopo[0].banner_tablet != ''}
-								<img
-									src={HOST_API + bannersTopo[0].banner_tablet}
-									alt={bannersTopo[0].title}
-									title={bannersTopo[0].title}
-									width="100%"
-									class="b-tablet"
-								/>
-							{/if}
-							{#if bannersTopo[0].banner_mobile != ''}
-								<img
-									src={HOST_API + bannersTopo[0].banner_mobile}
-									alt={bannersTopo[0].title}
-									title={bannersTopo[0].title}
-									width="100%"
-									class="b-mobile"
-								/>
-							{/if}
-						</a>
-					{:else}
-						{@html bannersTopo[0].html}
-					{/if}
-				</div>
-			</div>
+			{#if bannersTopo[0].html == ''}
+				{#if bannersTopo[0].banner_pc != ''}
+					<a
+						href={bannersTopo[0].link}
+						target="_blank"
+						class="link"
+						on:click={() => {
+							window.open(bannersTopo[0].link, '_blank');
+						}}
+					>
+						<img
+							src={HOST_API + bannersTopo[0].banner_pc}
+							alt={bannersTopo[0].title}
+							title={bannersTopo[0].title}
+							width="100%"
+							class="b-pc"
+						/>
+					</a>
+				{/if}
+				{#if bannersTopo[0].banner_tablet != ''}
+					<a href={bannersTopo[0].link} target="_blank" class="link">
+						<img
+							src={HOST_API + bannersTopo[0].banner_tablet}
+							alt={bannersTopo[0].title}
+							title={bannersTopo[0].title}
+							width="100%"
+							class="b-tablet"
+						/>
+					</a>
+				{/if}
+				{#if bannersTopo[0].banner_mobile != ''}
+					<a href={bannersTopo[0].link} target="_blank" class="link">
+						<img
+							src={HOST_API + bannersTopo[0].banner_mobile}
+							alt={bannersTopo[0].title}
+							title={bannersTopo[0].title}
+							width="100%"
+							class="b-mobile"
+						/>
+					</a>
+				{/if}
+			{:else}
+				{@html bannersTopo[0].html}
+			{/if}
 		</div>
 	</section>
 {/if}
