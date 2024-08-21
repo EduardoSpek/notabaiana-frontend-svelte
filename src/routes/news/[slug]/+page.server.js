fd0159a320722b60c400b322946c4f5f80e321fa
Eduardo Spek <spektogran@gmail.com>
2024-08-21 20:45:37 -0300

    fix: Ajustado paragrafo do texto

--- src/routes/news/[slug]/+page.server.js
+++ src/routes/news/[slug]/+page.server.js
@@ -45,8 +45,9 @@ export async function load({ fetch, params, cookies }) {
 		item['text'] = item.text.replace(/<br><br><br><br><br><br>/g, '<br><br>');
 		item['text'] = item.text.replace(/<br><br><br><br><br>/g, '<br><br>');
 		item['text'] = item.text.replace(/<br><br><br><br>/g, '<br><br>');
-		item['text'] = item.text.replace(/<br><br> <br><br>/g, '<br><br>');
 		item['text'] = item.text.replace(/<br><br><br>/g, '<br><br>');
+		item['text'] = item.text.replace(/<br><br> <br><br> <br><br>/g, '<br><br>');
+		item['text'] = item.text.replace(/<br><br> <br><br>/g, '<br><br>');
 	}
 
 	const token = cookies.get('user_token');
