c0bc8eb10ca2a275a5f45d290f8e84fe3468dc15
Eduardo Spek <spektogran@gmail.com>
2024-10-21 01:40:32 -0300

    Revert "refact: Google Recaptcha in login page"
    This reverts commit 749053fb7be8170a80a43d65c9e9d146c7a352b9.

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,8 +6,6 @@ const HOST = import.meta.env.VITE_HOST;
 const SITE_NAME = "Nota Baiana";
 const IMG_PADRAO = `${HOST}/notabaiana_400.jpg`;
 
-const KEY_RECAPTCHA = import.meta.env.VITE_GOOGLE_CAPTCHA;
-
 const formatarData = (data) => {
 	const data_news = new Date(Date.parse(data));
 	const dia = data_news.getDate().toString().padStart(2, "0");
@@ -34,5 +32,4 @@ export {
 	IMG_PADRAO,
 	globalStore,
 	bigFirstLetter,
-	KEY_RECAPTCHA,
 };
